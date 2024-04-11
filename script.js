fb888c2c8f2e9ed4177442d93215ef17e9dc1427
Vishnu Teja N J S <146103796+VishnuTejaNJS@users.noreply.github.com>
2024-04-11 19:40:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+var random=Math.floor(Math.random() *100+1);
+document.querySelector('.numOfPosts').innerHTML=random;
+document.querySelector('.followerCount').innerHTML=Math.floor(Math.random() * 10000);
+document.querySelector('.following').innerHTML=Math.floor(Math.random()*100+1);
